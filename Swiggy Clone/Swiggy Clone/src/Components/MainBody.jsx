5fe08e68cd50322121f51ade55f52c79b1e97df5
Aditya-Karki03 <adityakarki201@gmail.com>
2024-03-04 16:23:39 +0530

    Update

--- Swiggy Clone/Swiggy Clone/src/Components/MainBody.jsx
+++ Swiggy Clone/Swiggy Clone/src/Components/MainBody.jsx
@@ -5,9 +5,11 @@ import { FaArrowCircleLeft } from "react-icons/fa";
 import TopRestaurantCard from "./TopRestaurantCard";
 
 export default function MainBody(){
+    
     const[whatsOnYourMind,setWhatsOnYourMind]=useState([]);
     const[topRestro,setTopRestro]=useState([]);
-    // const[onlySixFoods,setOnlySixFoods]=useState([]);
+    // const[itemsToBeDisplayed,setItemsToBeDisplayed]=useState([])
+    
     useEffect(()=>{
         APICallForMainBody()
     },[])
@@ -16,21 +18,24 @@ export default function MainBody(){
         const json=await data.json();
         setWhatsOnYourMind(json.data.cards[0].card.card.imageGridCards.info);
         setTopRestro(json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants);
+     
     }
+   
+   
     console.log(topRestro)
-   
-   
+
+ 
 
     return(
-        <div className="w-screen mt-[100px] py-5 flex flex-col justify-center items-center ">
-            <div className="flex justify-between px-6 w-[961px] items-center ">
-                <h2 className="font-bold text-[20px]">Whats on your mind?</h2>
-                <div className="flex gap-2 text-[25px]  ">
+        <div className="w-screen mt-[100px] py-5  flex flex-col justify-center items-center ">
+            <div className="flex justify-between px-6 w-[961px]  items-center ">
+                <h2 className="font-bold text-[20px]">Top restaurant chains in Guwahati</h2>
+                <div className="flex gap-2 text-[25px] py-8  ">
                     <div className="hover:text-OnHoverColor hover:cursor-pointer"><FaArrowCircleLeft   /></div>
                     <div className="hover:text-OnHoverColor hover:cursor-pointer"><FaArrowCircleRight /></div>                 
                 </div>
             </div>
-            <div className="w-[961px] flex justify-center  overflow-hidden border-b border-b-[lightgray]">
+                <div className="w-[961px] flex justify-center  overflow-hidden border-b border-b-[lightgray] py-4">
             {
                 whatsOnYourMind.length==0?<ShimmerForWhatsOnYourMind/>:
                 whatsOnYourMind.map((item)=>{
@@ -40,16 +45,28 @@ export default function MainBody(){
             </div>
             <div className="flex justify-between px-6 w-[961px] items-center ">
                 <h2 className="font-bold text-[20px]">Top restaurant chains in Guwahati</h2>
-                <div className="flex gap-2 text-[25px]  ">
+                <div className="flex gap-2 text-[25px] py-8  ">
                     <div className="hover:text-OnHoverColor hover:cursor-pointer"><FaArrowCircleLeft   /></div>
                     <div className="hover:text-OnHoverColor hover:cursor-pointer"><FaArrowCircleRight /></div>                 
                 </div>
             </div>
+             
+            <div className="w-[961px]  flex flex-wrap justify-center items-center gap-10 overflow-hidden">
             {
-                topRestro.map((restro)=>{
-                    <TopRestaurantCard key={restro.info.id} name={restro.info.name} imageId={restro.info.cloudinaryImageId} rating={restro.info.avgRatingString} time={restro.info.sla.slaString} locality={restro.info.locality} areaName={restro.info.areaName}  />
-                })
+                topRestro.map((restro)=>  <TopRestaurantCard
+                                            key={restro.info.id}
+                                            name={restro.info.name} 
+                                            imageId={restro.info.cloudinaryImageId} 
+                                            rating={restro.info.avgRatingString} 
+                                            time={restro.info.sla.slaString} 
+                                            locality={restro.info.locality} 
+                                            areaName={restro.info.areaName}  
+                                            />
+                )
             }
-        </div>
+            
+       </div>
+    </div>
+       
     )
 }
--- Swiggy Clone/Swiggy Clone/src/Components/TopRestaurantCard.jsx
+++ Swiggy Clone/Swiggy Clone/src/Components/TopRestaurantCard.jsx
@@ -1,28 +1,26 @@
 import { MdStarRate } from "react-icons/md";
-//name={restro.info.name} imageId={restro.info.cloudinaryImageId} rating={restro.info.avgRatingString} time={restro.info.sla.slaString} locality={restro.info.locality} areaName={restro.info.areaName}
-export default function TopRestaurantCard({name,imageId,rating,time,locality, areaName}){
+
+export default function TopRestaurantCard(props){
     return(
-        <div className="w-[961px] flex justify-center overflow-hidden border-b border-b-[lightgray]">
-                <div className="h-[300px] hover:cursor-pointer hover:scale-90 transition ease-in-out ">
-                    <div className={"bg-[url('https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/f01666ac73626461d7455d9c24005cd4')] shadow-inner bg-cover bg-center h-[150px] w-[250px] rounded-xl"}>
-                        
-                    </div>
+        <div className="h-[300px] hover:cursor-pointer hover:scale-90 transition ease-in-out border-b border-b-[lightgray] ">
+                <div >
+                        <img src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/${props.imageId}`} alt="logo" className="h-[182px] w-[273px] rounded-xl" />                
+                </div>
                     <div className=" ">
-                        <p className="font-bold text-[20px]">{name}</p>
+                        <p className="font-bold text-[20px] ">{props.name.length>25?props.name.substring(0,25)+'...':props.name}</p>
                         <div>
                             <div className="flex gap-1 items-center">
                                 <div className="w-[20px] h-[20px] rounded-[50%] bg-[green] flex justify-center items-center text-[white]">
                                     <MdStarRate />
                                 </div>
-                                <span className="font-medium">4.5 . 25-30 mins</span>
+                                <span className="font-medium">{props.rating} . {props.time}</span>
                             </div>
                             <div className="text-[gray] font-normal">
-                                <p>Sweets, South Indian</p>
-                                <p>Fancy Bazar</p>
+                                <p>Sweets, {props.locality}</p>
+                                <p>{props.areaName}</p>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
     )
 }
