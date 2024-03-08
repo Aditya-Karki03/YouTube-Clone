9eae73670e2b3a0ee17de051743c1e0c8a819557
Aditya-Karki03 <adityakarki201@gmail.com>
2024-03-08 10:25:01 +0530

    Finally recommendation menu can be displayed for the particulart restaurant can be displayed if clicked

--- Swiggy Clone/Swiggy Clone/src/Components/RestroMenu.jsx
+++ Swiggy Clone/Swiggy Clone/src/Components/RestroMenu.jsx
@@ -1,52 +1,56 @@
 import { useEffect,useState } from "react";
 import { useParams } from "react-router-dom"
+import ShimmerForWhatsOnYourMind from "./ShimmerForWhatsOnYourMind";
+
 
 export default function RestroMenu(){
-    const[allMenus,setAllMenus]=useState([]);
-    const { restroId } = useParams();
+    const {restroId}=useParams();
+    const[multipleData,setMultipleData]=useState(null);
     
     useEffect(()=>{
-        ListOfMenuAPICalling();
+        apiCallForFetchingMenus();
     },[])
-    
 
-
-    const ListOfMenuAPICalling=async()=>{
-        const data=await fetch(`https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=26.1811818&lng=91.749661&restaurantId=${restroId}&catalog_qa=undefined&submitAction=ENTER`);
-        const menus=await data.json();
-        setAllMenus(menus.data.cards[2].groupedCard.cardGroupMap.REGULAR.cards[1].card.card.itemCards || menus.data.cards[2].groupedCard.cardGroupMap.cards[2].card.card.itemCards);
-
+    const apiCallForFetchingMenus=async()=>{
+        const data= await fetch(`https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=26.1811818&lng=91.749661&restaurantId=${restroId}&catalog_qa=undefined&submitAction=ENTER`);
+        const menuItems=await data.json();
+        setMultipleData(menuItems.data.cards);
+        // setMenu(menuItems.data.cards[2].groupedCard.cardGroupMap.REGULAR.cards[1].card.card.itemCards || menuItems.data.cards[2].groupedCard.cardGroupMap.REGULAR.cards[2].card.card.itemCards )
     }
-    
-    // const{ card }=allMenus.cards[2].groupedCard.cardGroupMap.REGULAR.cards[1].card;
-    console.log(allMenus)
    
+    
+    if(multipleData==null)    return <ShimmerForWhatsOnYourMind/>
+    const{name}=multipleData[0]?.card?.card?.info;
+    const{cards}=multipleData[2]?.groupedCard?.cardGroupMap?.REGULAR;
+    const recommended=cards.filter((item)=>item.card.card.title=='Recommended');
+    
+    // let itemCards=[]
+    // if(recommended) itemCards=recommended[0].card.card;
+    // console.log(itemCards)
+    const {itemCards}=recommended[0].card.card;
+    console.log(itemCards)
+    
+//     console.log(multipleData[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card);
+// console.log(multipleData[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card);
 
 
 
-    return(
+    return (
         <div className="mt-[130px]">
-            {/* <h1>{allMenus?.cards[0]?.card?.card?.info?.name}</h1> */}
-           
-            {/* <h2>{card.title}</h2> */}
-                <ul>
-                    <li>Gond Laddu</li>
-                    {/* {
-                            card.itemCards.map((item)=>
-                                <li key={item.card.info.id}>{item.card.info.name}</li>
-                            )
-                    } */}
-                </ul>
-            <h2>Sweets</h2>
-                <ul>
-                    <li>Ras Gulab Jamun</li>
-                    <li>Badam Anjir Bake</li>
-                    <li>Besan Laddu</li>
-                    <li>Badam Barfi</li>
-                    <li>Badam Gulli</li>
-                    <li>Banarasi Laddu</li>
-                    <li>Kaju Anjir Chakra</li>
-                </ul>
-        </div>
+            <h1>{name}</h1>
+            <h2 className="font-bold">Recommendation</h2>
+       
+            <ul>
+                
+                {
+                    itemCards.map((item)=>
+                        <li key={item.card.info.id}>
+                            {item.card.info.name}
+                        </li>
+                    )
+                }
+            </ul>
+        
+    </div>
     )
 }
