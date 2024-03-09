4fc4306586797f3ea8fe7c4dfda0c90052acec0b
Aditya-Karki03 <adityakarki201@gmail.com>
2024-03-09 22:23:24 +0530

    Update

--- Swiggy Clone/Swiggy Clone/src/Components/FoodItems.jsx
+++ Swiggy Clone/Swiggy Clone/src/Components/FoodItems.jsx
@@ -1,51 +1,61 @@
 import { IoIosArrowDropdown } from "react-icons/io";
+import { IoIosArrowDropup } from "react-icons/io";
 import { LuSquareDot } from "react-icons/lu";
 import { useRestroAPI } from "../Utils/useRestroAPI";
 import ShimmerForSearch from "./ShimmerForSearch";
+import { useState } from "react";
+
 
 
 
 export default function FoodItems(props){
-    const { id, title, lengthItem,recommendedValues } = props;
-// console.log(lengthItem ? lengthItem.length : "LengthItem is undefined");
-    console.log(recommendedValues)
- 
+    const {id, recommendedValues=null} = props;
+
+    // const[secondTimeClicked,setSecondTimeClicked]=useState(indexNo)
+
+    
+
+    // const[itemClicked,setItemClicked]=useState(false);
+    // function handleClicks(){
+    //     if(itemClicked==ind)
+    // }
+
     const restroInfo=useRestroAPI(id);
     if(!restroInfo) return <ShimmerForSearch/>
 
-    const {cards}=restroInfo[2].groupedCard.cardGroupMap.REGULAR;
- 
 
-    // const recommended=cards.filter((item)=>item.card.card.title);
-  
-    let {itemCards}=recommendedValues.card.card||null;
-    const{carousel}=recommendedValues.card.card||null
+    // function handleAccordion(){
+    //     setVisibility(!visibility)
+    // }
 
     return(
-        <div className="">
-            <div className="">
-                <p className="flex justify-between items-center">{`${title} (${lengthItem.length})`} <IoIosArrowDropdown /></p>
-            </div>
+        <div className="cursor-pointer">
+            {/* <div className="" onClick={handleAccordion}>
+                <p className="flex justify-between items-center font-bold ">{`${title} (${lengthItem.length})`} {(visibility)?<IoIosArrowDropup />:<IoIosArrowDropdown />}</p>
+            </div> */}
             <div className="">
                 {
                     
-                    itemCards && itemCards.map((item)=>{
+                     recommendedValues && recommendedValues.card.card.itemCards.map((item)=>{
                         return(
-                            <div key={item.card.info.id} className=" px-5 py-10 border-b border-[lightgray]">
+                            <div key={item.card.info.id} className="border-b border-[lightgray]">
+                                <div  className=" px-5 py-10 ">
                                 <LuSquareDot className={item.card.info.itemAttribute.vegClassifier=='VEG'?'text-[green]':'text-[red]'} />
                                 <p>{item.card.info.name}</p>
                                 <p>{`₹${(item.card.info.price)/100}`}</p>
                                 <br />
                                 <p>{item.card.info.description}</p>
                             </div>
+                                <div>
+                                        {/* image */}
+                                        <button>ADD</button>
+                                </div>
+                            </div>
                         )
                     })
                 }
                 
-                <div>
-                    {/* image */}
-                    <button>ADD</button>
-                </div>
+                
             </div>
         </div>
     )
--- Swiggy Clone/Swiggy Clone/src/Components/RestroMenu.jsx
+++ Swiggy Clone/Swiggy Clone/src/Components/RestroMenu.jsx
@@ -5,10 +5,20 @@ import { FaStar } from "react-icons/fa";
 import { IoIosBicycle } from "react-icons/io";
 import FoodItems from "./FoodItems";
 
+import { IoIosArrowDropdown } from "react-icons/io";
+import { IoIosArrowDropup } from "react-icons/io";
+
 
 export default function RestroMenu(){
     const {restroId}=useParams();
     const[multipleData,setMultipleData]=useState(null);  
+    const[whichItemClicked,setWhichItemClicked]=useState(null);
+    const[itemIsClicked,setItemIsClicked]=useState(false);
+
+    const[indexValue,setIndexValue]=useState(null)
+
+    
+
     useEffect(()=>{
         apiCallForFetchingMenus();
     },[])
@@ -17,18 +27,25 @@ export default function RestroMenu(){
         const data= await fetch(`https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=26.1811818&lng=91.749661&restaurantId=${restroId}&catalog_qa=undefined&submitAction=ENTER`);
         const menuItems=await data.json();
         setMultipleData(menuItems.data.cards);
+        setItemIsClicked(!itemIsClicked)
         // setMenu(menuItems.data.cards[2].groupedCard.cardGroupMap.REGULAR.cards[1].card.card.itemCards || menuItems.data.cards[2].groupedCard.cardGroupMap.REGULAR.cards[2].card.card.itemCards )
     }
    
     
     if(multipleData==null)    return <ShimmerForWhatsOnYourMind/>
-    const{name,cuisines,locality,sla,feeDetails,avgRating,totalRatingsString}=multipleData[0]?.card?.card?.info;
+    const{name,cuisines,locality,sla,feeDetails,avgRating,totalRatingsString}=multipleData[0]?.card?.card?.info || null;
     
     const {cards}=multipleData[2].groupedCard.cardGroupMap.REGULAR;
  
 
     const recommended=cards.filter((item)=>item.card.card.title && (item.card.card.itemCards  || item.card.card.carousel));
     
+    function handleAccordion(index){
+        setWhichItemClicked(index)
+        if(indexValue==index) setIndexValue(null);
+        else    setIndexValue(index)        
+    }
+    let visibility=true;
 
     return (
       <div className="mt-[110px] px-24  ">
@@ -50,10 +67,13 @@ export default function RestroMenu(){
        
             {
                     //let {title}=recommended[0].card.card||null;
-                    recommended.map((item,index)=>{
+                     recommended.map((item,index)=>{
                         return (
-                            <div key={index}  className="  py-5 border-b-[10px] border-[#e1e0e0]">
-                                <FoodItems id={restroId} title={item.card.card.title} recommendedValues={recommended[index]} lengthItem={item.card.card.itemCards ||item.card.card.carousel} />
+                            <div key={index}   className=" mt-4  py-5 border-b-[10px] border-[#e1e0e0]">
+                                <div className="cursor-pointer" onClick={()=>handleAccordion(index)}>
+                                    <p className="flex justify-between items-center font-bold ">{`${item.card.card.title} (${item.card.card.itemCards.length ||item.card.card.carousel.length})`} {(visibility)?<IoIosArrowDropup />:<IoIosArrowDropdown />}</p>
+                                </div>
+                               {indexValue==index && <FoodItems indexNo={indexValue}  id={restroId} title={item.card.card.title}  recommendedValues={recommended[whichItemClicked]} lengthItem={item.card.card.itemCards ||item.card.card.carousel} />}
                             </div>
                         )
                     })
