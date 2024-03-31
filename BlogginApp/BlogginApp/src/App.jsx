5177cf79550f9e2d010698c0078093ae3143ed77
Aditya-Karki03 <adityakarki201@gmail.com>
2024-03-31 19:21:33 +0530

    Components

--- BlogginApp/BlogginApp/src/App.jsx
+++ BlogginApp/BlogginApp/src/App.jsx
@@ -1,10 +1,30 @@
-
+import { Provider, useDispatch } from "react-redux"
+import { store } from "./Store/store"
+import { useState, useEffect } from "react"
+import authService from "./Appwrite/auth";
+import { login, logout } from "./Store/authSlice";
 
 function App() {
-
+  const dispatch=useDispatch()
+  const[loading,setLoading]=useState(true);
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then((user)=>{
+        if(user)  dispatch(login(user))
+        else dispatch(logout());
+      }
+    )
+    .finally(()=>setLoading(false))
+  },[])
+  
 
   return (
    <>
+    
+    {
+      loading?(<h1>Component is loading</h1>):null
+    }
+
    </>
   )
 }
