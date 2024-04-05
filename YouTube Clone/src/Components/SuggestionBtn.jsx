70ffb01d1bb2fb6c8fabe40ededca79317ad13be
Aditya-Karki03 <adityakarki201@gmail.com>
2024-04-05 18:27:44 +0530

    Add slider for sliding functionality

---
+++ YouTube Clone/src/Components/SuggestionBtn.jsx
@@ -0,0 +1,28 @@
+// import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+export default function SuggestionBtn(){
+    const btnOptions=['All','Music','News','Mixes','Computer Science','Bollywood Music','Indian pop music','Soundtracks','Indie Music','Cars','Comedy','Rock Music'];
+    const settings = {
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 8,
+        slidesToScroll: 3
+      };
+    return(
+        <div className="z-10 shadow-xl mt-20 ml-20 py-1 px-9 h-8 sticky top-16  bg-white">
+            <Slider {...settings}>
+               {
+                 btnOptions.map((items)=>  <button className="px-2 py-1 transition-all text-[13px] rounded-lg hover:bg-[lightgray]" key={items}>{items}</button>)
+               }
+                
+            </Slider>
+        </div>
+    )
+}
+
+
+
